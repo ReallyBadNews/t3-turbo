091bd5035e684401478711d51e73b791736faf03
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-11-21 21:25:54 -0500

    lint

--- apps/nextjs/src/pages/api/auth/[...nextauth].ts
+++ apps/nextjs/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 
-import { authOptions } from '@badnews/auth';
+import { authOptions } from "@badnews/auth";
 
 export default NextAuth(authOptions);
