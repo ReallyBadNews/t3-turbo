266442b5b9eb4695901e1e4d0b7a0f5ec94f44c5
Kenny Elshoff <kelshoff@grahamdigital.com>
2023-01-04 21:25:07 -0500

    update serverSchema

--- apps/nextjs/src/env/schema.mjs
+++ apps/nextjs/src/env/schema.mjs
@@ -6,7 +6,25 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
+  DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url()
+  ),
+  CLOUDINARY_URL: z.string(),
+  CLOUDINARY_BASE_PUBLIC_ID: z.string(),
+  IDENTITY_SBX:
+    process.env.NODE_ENV === "production" ? z.string().optional() : z.string(),
+  IDENTITY_PROD:
+    process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
 });
 
 /**
@@ -15,7 +33,7 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_BAR: z.string(),
+  // NEXT_PUBLIC_CLIENTVAR: z.string(),
 });
 
 /**
@@ -25,5 +43,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
