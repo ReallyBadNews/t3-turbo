9f6d246b32188062dcf200b83a6b44044bbe7fd6
Kenny Elshoff <github@kennyelshoff.com>
2023-03-25 23:33:38 -0400

    fix: invalidate byId when liking pin

--- apps/nextjs/src/components/Combobox.tsx
+++ apps/nextjs/src/components/Combobox.tsx
@@ -63,7 +63,7 @@ export const Combobox = ({ label, options }: ComboboxProps) => {
         <HUICombobox.Input
           className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(option) => (option as unknown as Option)?.name}
+          displayValue={(option) => (option as Option)?.name}
           name="community"
         />
         <HUICombobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
